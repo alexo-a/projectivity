8a70a400c9c80c6463f3fe78a41065a2be984895
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-11-08 14:51:43 -0600

    dashboard data populates correctly

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,107 @@
 import React from 'react';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_MY_TASKS } from "../utils/queries";
+
+function parseTasks(tasks) {
+    //keeps track of all the unique projects so the operations afterward are smoother
+    let uniqueProjects = new Set(tasks.map(task => {
+        return task.project._id
+    }));
+
+    let compilation = [...uniqueProjects].map(id => {
+        return {
+            projectId: id,
+            projectTitle: "",
+            projectDescription: "",
+            tasks: []
+        }
+    })
+
+    for (let i in tasks) {
+        //cleanly assemble data from the tasks
+        let taskInfo = {
+            projectId: tasks[i].project._id,
+            projectTitle: tasks[i].project.title,
+            projectDescription: tasks[i].project.description,
+            taskId: tasks[i]._id,
+            taskTitle: tasks[i].title,
+            taskDescription: tasks[i].description
+        }
+
+        for (let j in compilation) {
+            //try to find a match for the projectId so no duplicate projects show up when we group by project in the DOM
+            if (taskInfo.projectId === compilation[j].projectId) {
+                compilation[j].projectTitle = taskInfo.projectTitle;
+                compilation[j].projectDescription = taskInfo.projectDescription;
+                compilation[j].tasks.push({
+                    taskId: tasks[i]._id,
+                    taskTitle: tasks[i].title,
+                    taskDescription: tasks[i].description
+                })
+            }
+        }
+    }
+    return compilation;
+}
 
 function Dashboard() {
+
+    const userInfo = Auth.getUserInfo();
+    const username = userInfo.username;
+    const userId = userInfo._id;
+
+    //get the logged-in user's assigned tasks.
+    const { loading, data } = useQuery(QUERY_MY_TASKS,
+        { variables: { userId } } );
+
+    const tasks = data?.myTasks || {};
+    let results=[]
+    if (loading) {
+        return null
+    }
+    if (!loading) {
+        results = parseTasks(tasks)
+        //console.log(JSON.stringify(tasks))
+    }
+
+
     return(
         <div>
-            Dashboard
+            <h2>Dashboard</h2>
+
+            <h5>Your Current Tasks:</h5>
+
+            <div className="componentContainer">
+                <div className="kanbanContainer">
+
+                        {results ? (
+                            <div key="a">
+                                {results.map(project => (
+                                    <div className="project-container border-bottom" key={project.projectTitle}>
+                                        <div className="" key="projectTitle">
+                                            {project.projectTitle}
+                                        </div>
+                                        < div className="employee-task-container">
+                                            {project.tasks.map(task => (
+                                                <>
+                                                    <div className="" key={task.taskTitle}>
+                                                        {task.taskTitle}
+                                                    </div>
+                                                    <div className="">
+                                                        {/* TODO put button here to select task */}
+                                                    </div>
+                                                </>
+                                            ))}
+
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : null}
+
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/pages/EmployeeReport.js
+++ client/src/pages/EmployeeReport.js
@@ -4,32 +4,7 @@ import Auth from "../utils/auth";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_MY_TIMESHEETS } from "../utils/queries";
 import EmployeeReportChart from "../components/EmployeeReportChart"
-/*
-{
-    labels: ['January', 'February', 'March',
-        'April', 'May'],
-        datasets: [
-            {
-                label: 'Rainfall',
-                backgroundColor: [
-                    '#B21F00',
-                    '#C9DE00',
-                    '#2FDE00',
-                    '#00A6B4',
-                    '#6800B4'
-                ],
-                hoverBackgroundColor: [
-                    '#501800',
-                    '#4B5000',
-                    '#175000',
-                    '#003350',
-                    '#35014F'
-                ],
-                data: [65, 59, 80, 81, 56]
-            }
-        ]
-}
-*/
+
 function processTimeSheets(timesheets) {
     //processed the time sheets, extracting the project info, task info, and duration info.
     //logs this data as objects in the compilation array
