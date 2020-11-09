eb7cba1bc537016d5ca62911fbacab54aa2a6cd4
laurenceokite <ljsokite@gmail.com>
2020-11-08 20:41:25 -0600

    serviceWorker.js location?

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -11,7 +11,55 @@ export function formatTimeSpan(timeSpan) {
 
 export function getCurrentWeekInfo() {
     const weekNumber=moment().week();
-    const weekStartDate=moment().startOf("week").format("D MMMM YYYY")
+    const weekStartDate=moment().startOf("week").format("Do MMMM YYYY")
 
     return {weekStartDate, weekNumber}
 }
+
+export function idbPromise(storeName, method, object) {
+    return new Promise((resolve, reject) => {
+      const request = window.indexedDB.open('Projectivity', 1);
+      let db, tx, store;
+      request.onupgradeneeded = function(e) {
+        const db = request.result;
+        //db.createObjectStore('dashboard', { keyPath: '_id' });
+      };
+  
+      request.onerror = function(e) {
+        console.log('There was an error');
+      };
+  
+      request.onsuccess = function(e) {
+        db = request.result;
+        tx = db.transaction(storeName, 'readwrite');
+        store = tx.objectStore(storeName);
+  
+        db.onerror = function(e) {
+          console.log('error', e);
+        };
+  
+        switch (method) {
+          case 'put':
+            store.put(object);
+            resolve(object);
+            break;
+          case 'get':
+            const all = store.getAll();
+            all.onsuccess = function() {
+              resolve(all.result);
+            };
+            break;
+          case 'delete':
+            store.delete(object._id);
+            break;
+          default:
+            console.log('No valid method');
+            break;
+        }
+  
+        tx.oncomplete = function() {
+          db.close();
+        };
+      };
+    });
+  }
