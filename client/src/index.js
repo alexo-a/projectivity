5264517366279f008a47728f814d9cbea0926451
laurenceokite <ljsokite@gmail.com>
2020-11-08 19:20:34 -0600

    registered service worker

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  serviceWorker.register()
 );
 
 // If you want to start measuring performance in your app, pass a function
