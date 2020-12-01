f8e6606035d1a0f32ed29109788d886781ff06c4
Alex Ostrowski <a.ostrowski.11@gmail.com>
2020-12-01 15:37:52 -0600

    fixed projectreport if no timestamps logged yet

--- client/src/components/ProjectReport/index.js
+++ client/src/components/ProjectReport/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {createProjectReportPDF } from "../../utils/helpers"
+import { createProjectReportPDF } from "../../utils/helpers"
 //import Auth from "../../utils/auth";
 import { useQuery } from "@apollo/react-hooks";
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { QUERY_PROJECT_TIMESHEETS } from "../../utils/queries";
 import moment from "moment";
 
 function processProjectTimeSheets(timesheets) {
-    
+
     let compilation = [];
     //use a Set to find the unique "timesheet.task._id"s quickly
     let uniqueTasks = new Set(timesheets.map(timesheet => {
@@ -17,12 +17,12 @@ function processProjectTimeSheets(timesheets) {
     //set up the objects to house all our necessary data
     compilation = [...uniqueTasks].map(id => {
         return {
-            taskId: id, 
-            taskTitle: "", 
-            users: [], 
+            taskId: id,
+            taskTitle: "",
+            users: [],
             totalTime: 0,
-            status: "In Progress" 
-        } 
+            status: "In Progress"
+        }
     })
     //loop through timesheets ...
     for (let i in timesheets) {
@@ -50,9 +50,9 @@ function processProjectTimeSheets(timesheets) {
                 }
                 //... or adding new user data if no timesheet exists for them for that task
                 if (!timesheetLogged) {
-                    compilation[j].users.push({ 
-                        username: log.username, 
-                        duration: log.duration 
+                    compilation[j].users.push({
+                        username: log.username,
+                        duration: log.duration
                     })
                 }
             }
@@ -63,7 +63,7 @@ function processProjectTimeSheets(timesheets) {
 
 function ProjectReport() {
 
-    const { id: projectId , title} = useParams();
+    const { id: projectId, title } = useParams();
 
     const { loading, data } = useQuery(QUERY_PROJECT_TIMESHEETS,
         {
@@ -84,51 +84,51 @@ function ProjectReport() {
         compilationInfo = processProjectTimeSheets(timesheets)
     }
 
-    function generatePDF(data){
+    function generatePDF(data) {
         createProjectReportPDF(data, title)
     }
 
     return (
+
         <div>
-
-            <div className="text-center">
-                <button onClick={() => {generatePDF(compilationInfo)}}>Download PDF Version</button>
-            </div>
-            
-            <h2 className="text-center" id="projectName">{title}</h2>
-            <h3 className="text-center" id="reportDescription">Project Progress Report</h3>
-            <h3 className="text-center" id="date">As of {today}</h3>
-            {/*<h5 className="text-center">Task View</h5>*/}
-
-            <div className="">
-
-                <div className="table-title border-bottom bg-dark text-light bold align-bottom">
-                    <div className="text-mid">
-                        Task
+            {compilationInfo ? (
+                <>
+                    <div className="text-center">
+                        <button onClick={() => { generatePDF(compilationInfo) }}>Download PDF Version</button>
                     </div>
-                    <div className="text-center text-mid">
-                        Status
-                    </div>
-                    <div className="text-center text-mid">
-                        Employee Name
-                    </div>
-                    <div className="text-center text-mid">
-                        Hours
-                    </div>
-                </div>
 
-                {compilationInfo ? (
-                    <>
-                        {compilationInfo.map(task => (
-                            <div className="task-container py-2 border-bottom" key={task.taskId}>
-                                <div className="title-box">
-                                    {task.taskTitle}
-                                </div>
-                                <div className="text-center">
-                                    {task.status}
-                                </div>
-                                <div className="employee-container">
-                                    {task.users.map(taskUser =>  (
+                    <h2 className="text-center" id="projectName">{title}</h2>
+                    <h3 className="text-center" id="reportDescription">Project Progress Report</h3>
+                    <h3 className="text-center" id="date">As of {today}</h3>
+                    {/*<h5 className="text-center">Task View</h5>*/}
+
+                    <div className="">
+
+                        <div className="table-title border-bottom bg-dark text-light bold align-bottom">
+                            <div className="text-mid">
+                                Task
+                            </div>
+                            <div className="text-center text-mid">
+                                Status
+                            </div>
+                            <div className="text-center text-mid">
+                                Employee Name
+                            </div>
+                            <div className="text-center text-mid">
+                                Hours
+                            </div>
+                        </div>
+                        <>
+                            {compilationInfo.map(task => (
+                                <div className="task-container py-2 border-bottom" key={task.taskId}>
+                                    <div className="title-box">
+                                        {task.taskTitle}
+                                    </div>
+                                    <div className="text-center">
+                                        {task.status}
+                                    </div>
+                                    <div className="employee-container">
+                                        {task.users.map(taskUser => (
                                             <React.Fragment key={taskUser.username}>
                                                 <div className="text-center">
                                                     {taskUser.username}
@@ -138,18 +138,22 @@ function ProjectReport() {
                                                 </div>
                                             </React.Fragment>
                                         )
-                                    )}
-                                    <div></div>
-                                    <div className="employee-total text-center bold">
-                                        {task.totalTime.toFixed(2)}
+                                        )}
+                                        <div></div>
+                                        <div className="employee-total text-center bold">
+                                            {task.totalTime.toFixed(2)}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </>
-                ) : null}
-            </div>
+                            ))}
+                        </>
+
+                    </div>
+                </>
+            ) : 
+            <h1>There are no timestamps logged for this project yet.</h1>}
         </div >
+
     )
 }
 
